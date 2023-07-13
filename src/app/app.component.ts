3cd4abdd162f44a5c6d548f32f463edcca43e8ce
Evans-254 <71435810+Evans-254@users.noreply.github.com>
2023-07-13 09:35:34 +0300

    product management system first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+selector: 'app-root',
+templateUrl: './app.component.html',
+ styleUrls: ['./app.component.css']
+
+
+ 
+// template:`
+// <div>
+//   <h1>{{title}}</h1>
+//   <!-- <div>Hello Mogeni</div> -->
+//   <app-products> </app-products>
+// </div>
+//  `
 })
-export class AppComponent {
-  title = 'dashboard';
+
+
+export class AppComponent{
+  title: string = 'ACME Product Management';
 }
---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-products',
+    templateUrl:'./product-list.component.html'
+}) 
+
+export class ProductListComponent{
+    title: string = 'Products'
+}
